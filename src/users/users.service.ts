4860bbff5db1738a4d8de3f5c1515c9cd7ebbbb8
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-17 12:26:39 +0600

    ...

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectRepository(UserEntity) private usersRepository:Repository<UserEntity>) {}
+
+  async signup(body:any){
+    const newUser = this.usersRepository.create(body);
+    await this.usersRepository.save(newUser);
+    return newUser;
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
