32c9a93e1aa03a992e9e5a7ed353c7de8c2518b8
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-17 11:42:40 +0600

    PostgreSQL & TypeORM setup done

---
+++ db/data-source.ts
@@ -0,0 +1,21 @@
+import { DataSource, DataSourceOptions} from 'typeorm'
+import { config }  from 'dotenv';
+
+config();
+
+export const dataSourceOptions:DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT!),
+  username: process.env.DB_USERNAME ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
+  synchronize: true,
+  logging: false,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/db/migrations/*{.ts,.js}'],
+}
+
+const dataSource = new DataSource(dataSourceOptions)
+
+export default dataSource
